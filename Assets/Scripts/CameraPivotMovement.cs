ff8f136e241053c148ecb86b6ed2aad578e5fdef
Sergio Vargas <sergiovargas9@gmail.com>
2015-01-02 21:09:34 -0300

    Agregados Assets de unity + Scripts

---
+++ Assets/Scripts/CameraPivotMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraPivotMovement : MonoBehaviour {
+
+	public List<Transform> wayPoints;
+	public int actualWayPoint = 0; 
+
+	public Transform pivot;
+
+	//CAMBIAR POR CORUTINAS
+	private Transform target;
+	public float speed = 10;
+
+	void Start(){
+			
+		/*GameObject[] ga = GameObject.FindGameObjectsWithTag ("WayPoint");
+
+		foreach (GameObject ob in ga) {
+			wayPoints.Add(ob.transform);
+		}*/
+
+		target = transform;
+	}
+
+	public void NextWayPoint(){
+		actualWayPoint++;
+		if (actualWayPoint >= wayPoints.Count)
+			actualWayPoint = 0;
+
+		target = wayPoints [actualWayPoint];
+	}
+
+	public void PreviousWayPoint(){
+		actualWayPoint--;
+		if (actualWayPoint <= 0)
+			actualWayPoint = wayPoints.Count;
+		
+		target = wayPoints [actualWayPoint];
+	}
+
+	void Update(){
+
+		transform.position = Vector3.Lerp (transform.position, target.position, speed * Time.deltaTime);
+
+		//OJO
+		transform.LookAt (pivot);
+	}
+
+
+
+}
